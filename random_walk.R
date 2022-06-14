d8042574fe94c9c2553ea1690c2ad14c80be5bf0
Travis Hinkelman <travis.hinkelman@gmail.com>
2022-06-14 01:48:39 -0700

    Finish adding R examples to random_walk.R

--- random_walk.R
+++ random_walk.R
@@ -16,7 +16,6 @@ random_walk_c <- function(n = 1000) {
     steps
 }
 
-
 # vectorized
 random_walk_v <- function(n = 1000) {
     steps <- sample(c(-1, 1), size = n, replace = TRUE)
@@ -29,30 +28,62 @@ bench::mark(
     random_walk_v(),
     check = FALSE)
 
-# current version below is relatively straightforward translation of python version
-# see this link for multiple other options: https://stat.ethz.ch/pipermail/r-help/2012-February/303756.html
+# find_crossing_1 is a translation of python version
+# following link provides multiple other options:
+# https://stat.ethz.ch/pipermail/r-help/2012-February/303756.html
+# included a few for testing here
 
-find_crossing_1 <- function(seq, sub){
+find_crossing_1 <- function(seq, sub) {
+    n <- length(seq)
+    m <- length(sub)
     out <- c()
-    sublen <- length(sub) - 1    # adjustment for 1-based indexing
-    for (i in seq_len(length(seq) - sublen)) {
-        if (identical(seq[i:(i + sublen)], sub)) out <- c(out, i)
+    for (i in seq_len(n - m + 1)) {
+        if (identical(seq[i:(i + m - 1)], sub)) out <- c(out, i)
     }
     out
-}
+    }
 
-occur1 <- function(exmpl, patrn) {
-    m <- length(patrn)
-    n <- length(exmpl)
-    candidate <- seq_len(n-m+1)
+# slightly modified version of occur1 from link
+# it is iteratively filtering candidate
+find_crossing_2 <- function(seq, sub) {
+    n <- length(seq)
+    m <- length(sub)
+    candidate <- seq_len(n - m + 1)
     for (i in seq_len(m)) {
-        candidate <- candidate[patrn[i] == exmpl[candidate + i - 1]]
+        candidate <- candidate[sub[i] == seq[candidate + i - 1]]
     }
     candidate
+    }
+
+# slightly modified version of occur3 from link
+# reversing is based on how embed creates the matrix
+# vector formed by repeat can be compared directly to matrix, w
+# rowSums indicates if all columns match the values in sub
+find_crossing_3 <- function(seq, sub) {
+    w <- embed(seq, length(sub))
+    which(rowSums(w == rep(rev(sub), each = nrow(w))) == ncol(w))
   }
 
-W <- random_walk_v()
+# slightly modified version of occur4 from link
+# wasn't found to be very fast but I find it highly readable
+find_crossing_4 <- function(seq, sub) {
+    which(zoo::rollapply(seq, length(sub), identical, sub, fill = FALSE, align = "left"))
+  }
+
+# trying data.table version of rollapply
+# need to jump through some extra hoops because not able to work directly with logical vectors
+# roughly 10x as fast as zoo version but not nearly as fast as find_crossing_2
+find_crossing_5 <- function(seq, sub) {
+    identical_mod <- function(x, y) if (identical(x, y)) 1 else 0
+    x <- data.table::frollapply(seq, length(sub), identical_mod, sub, align = "left")
+    which(x == 1)
+  }
+
+w <- random_walk_v()
 bench::mark(
-    find_crossing_1(W, c(1, 0, -1)),
-    occur1(W, c(1, 0, -1))
+    find_crossing_1(w, c(1, 0, -1)),
+    find_crossing_2(w, c(1, 0, -1)),
+    find_crossing_3(w, c(1, 0, -1)),
+    find_crossing_4(w, c(1, 0, -1)),
+    find_crossing_5(w, c(1, 0, -1))
 )
